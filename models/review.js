f70cda543bbe71ac9ead17eb0c62831f2ee7631d
fng3r <dinvk@mail.ru>
2018-04-23 13:27:29 +0300

    default -> defaultValue

--- models/review.js
+++ models/review.js
@@ -11,6 +11,6 @@ module.exports = (sequelize, DataTypes) =>
         rating: { type: DataTypes.DOUBLE },
         isApproved: {
             type: DataTypes.BOOLEAN,
-            default: false
+            defaultValue: false
         }
     });
