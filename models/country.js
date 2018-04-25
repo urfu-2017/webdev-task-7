aef27e8e0277d78476410e1f713c614f33638e80
Reshetnikov Artem <mr.reshetnikov.artem.2017@mail.ru>
2018-04-25 11:12:35 +0500

    Ny i vse

--- models/country.js
+++ models/country.js
@@ -12,5 +12,9 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.TEXT,
             unique: true
         }
+    }, {
+        indexes: [{
+            fields: ['name']
+        }]
     });
 };
