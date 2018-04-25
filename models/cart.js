dd86f98fa68751bd45a9d6aca170c548bfae5d35
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2018-04-25 13:53:10 +0500

    allowNull

--- models/cart.js
+++ models/cart.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
         },
         userId: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allowNull: true,
             foreignKey: true,
             references: {
                 model: 'users',
