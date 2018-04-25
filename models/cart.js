b6bb8f2d21e8ab469b45300f5d04f2d40f44c971
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2018-04-25 13:09:01 +0500

    type integer

--- models/cart.js
+++ models/cart.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
             autoIncrement: true
         },
         userId: {
-            type: DataTypes.TEXT,
+            type: DataTypes.INTEGER,
             allowNull: false,
             foreignKey: true,
             references: {
