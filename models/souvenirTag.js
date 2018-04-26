46c156306c0a6e44ad27a7018618ced9f52db4f4
LevshinMisha <LevshinMisha@github.com>
2018-04-26 13:54:14 +0500

    забыл добавить

---
+++ models/souvenirTag.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => sequelize.define('souvenir_tags',
+    {
+        souvenirId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        tagId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        }
+    },
+    {
+        timestamps: true,
+        indexes: [
+            { fields: ['souvenirId', 'tagId'] }
+        ]
+    }
+);
