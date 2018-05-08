6543fb25c37c42b1819b8a9817cfe75199865b86
Anton Frehser <farnetsohn@gmail.com>
2018-05-08 19:24:13 +0500

    another nonNull fix [2]

--- models/review.js
+++ models/review.js
@@ -26,8 +26,7 @@ module.exports = (sequelize, DataTypes) => {
             references: {
                 model: 'users',
                 key: 'id'
-            },
-            allowNull: false
+            }
         }
     },
     {
