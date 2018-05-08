4b2eebc6c1a0ebf4d121e4caf7268debe6468228
Anton Frehser <farnetsohn@gmail.com>
2018-05-08 19:11:46 +0500

    queries

--- queries.js
+++ queries.js
@@ -5,60 +5,129 @@ class Queries {
         Object.assign(this, models);
     }
 
+    get op() {
+        return this.sequelize.Op;
+    }
+
     getAllSouvenirs() {
-        // Данный метод должен возвращать все сувениры.
+        return this.Souvenir.findAll();
     }
 
     getCheapSouvenirs(price) {
-        // Данный метод должен возвращать все сувениры, цена которых меньше или равна price.
+        return this.Souvenir.findAll({
+            where: {
+                price: { [this.op.lte]: price }
+            }
+        });
     }
 
     getTopRatingSouvenirs(n) {
-        // Данный метод должен возвращать топ n сувениров с самым большим рейтингом.
+        return this.Souvenir.findAll({
+            order: [
+                ['rating', 'DESC']
+            ],
+            limit: n
+        });
     }
 
     getSouvenirsByTag(tag) {
-        // Данный метод должен возвращать все сувениры, в тегах которых есть tag.
-        // Кроме того, в ответе должны быть только поля id, name, image, price и rating.
+        return this.Souvenir.findAll({
+            include: {
+                model: this.Tag,
+                where: { name: tag },
+                attributes: []
+            },
+            attributes: ['id', 'name', 'image', 'price', 'rating']
+        });
     }
 
     getSouvenirsCount({ country, rating, price }) {
-        // Данный метод должен возвращать количество сувениров,
-        // из страны country, с рейтингом больше или равной rating,
-        // и ценой меньше или равной price.
-
-        // Важно, чтобы метод работал очень быстро,
-        // поэтому учтите это при определении моделей (!).
+        return this.Souvenir.count({
+            where: {
+                rating: {
+                    [this.op.gte]: rating
+                },
+                price: {
+                    [this.op.lte]: price
+                }
+            },
+            include: {
+                model: this.Country,
+                where: { name: country }
+            }
+        });
     }
 
     searchSouvenirs(substring) {
-        // Данный метод должен возвращать все сувениры, в название которых входит
-        // подстрока substring. Поиск должен быть регистронезависимым.
+        return this.Souvenir.findAll({
+            where: {
+                name: {
+                    [this.op.iRegexp]: substring
+                }
+            }
+        });
     }
 
     getDisscusedSouvenirs(n) {
-        // Данный метод должен возвращать все сувениры, имеющих >= n отзывов.
-        // Кроме того, в ответе должны быть только поля id, name, image, price и rating.
+        return this.Souvenir.findAll({
+            include: {
+                model: this.Review,
+                attributes: []
+            },
+            order: ['id'],
+            group: ['souvenir.id'],
+            having: this.sequelize.where(
+                this.sequelize.fn('COUNT', this.sequelize.col('reviews.id')), '>=', n
+            ),
+            attributes: ['name', 'image', 'price', 'rating']
+        });
     }
 
     deleteOutOfStockSouvenirs() {
-        // Данный метод должен удалять все сувениры, которых нет в наличии
-        // (то есть amount = 0).
-
-        // Метод должен возвращать количество удаленных сувениров в случае успешного удаления.
+        return this.Souvenir.destroy({
+            where: { amount: 0 }
+        });
     }
 
-    addReview(souvenirId, { login, text, rating }) {
-        // Данный метод должен добавлять отзыв к сувениру souvenirId
-        // содержит login, text, rating - из аргументов.
-        // Обратите внимание, что при добавлении отзыва рейтинг сувенира должен быть пересчитан,
-        // и всё это должно происходить за одну транзакцию (!).
+    async addReview(souvenirId, { login, text, rating }) {
+        const user = await this.User.findOne({
+            where: { login }
+        });
+        const souvenir = await this.Souvenir.findById(souvenirId);
+
+        const review = {
+            userId: user.id,
+            text,
+            rating
+        };
+
+        const transaction = async transaction => {
+            const reviews = await souvenir.getReviews({ transaction });
+            const updatedRating = (reviews.length * souvenir.rating + rating) / (reviews.length + 1);
+
+            await souvenir.createReview(review, { transaction });
+            await souvenir.update({ updatedRating }, { transaction });
+        };
+
+        return this.sequelize.transaction(transaction);
     }
 
     getCartSum(login) {
-        // Данный метод должен считать общую стоимость корзины пользователя login
-        // У пользователя может быть только одна корзина, поэтому это тоже можно отразить
-        // в модели.
+        return this.Cart.sum(
+            'souvenirs.price',
+            {
+                include: [
+                    {
+                        model: this.User,
+                        where: { login }
+                    },
+                    {
+                        model: this.Souvenir
+                    }
+                ],
+                includeIgnoreAttributes: false
+            }
+        );
     }
 }
 
